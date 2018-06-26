a269b8bdf3e3834a1c01d7a7ff61e29a8f8e036a
IMakeGames <arbolperro@gmail.com>
2018-06-26 17:24:32 -0400

    - Made a good sql script FOR REAL

--- dbpostulacion.sql
+++ dbpostulacion.sql
@@ -1,41 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.8.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 25, 2018 at 11:58 PM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `dbpostulacion`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `comuna`
---
+CREATE DATABASE postulacion;
+USE postulacion;
 
 CREATE TABLE `comuna` (
   `comuna_id` int(11) NOT NULL,
   `comuna_nombre` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `comuna`
---
 
 INSERT INTO `comuna` (`comuna_id`, `comuna_nombre`) VALUES
 (0, 'Comuna de Santiago'),
@@ -71,42 +40,22 @@ INSERT INTO `comuna` (`comuna_id`, `comuna_nombre`) VALUES
 (30, 'Comuna de San Ramón'),
 (31, 'Comuna de Vitacura');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `educacione`
---
-
-CREATE TABLE `educacione` (
-  `educacione_id` int(11) NOT NULL,
-  `educacione_tipo` varchar(25) NOT NULL
+CREATE TABLE `educacion` (
+  `educacion_id` int(11) NOT NULL,
+  `educacion_tipo` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `educacione`
---
-
-INSERT INTO `educacione` (`educacione_id`, `educacione_tipo`) VALUES
+INSERT INTO `educacion` (`educacion_id`, `educacion_tipo`) VALUES
 (0, 'Profesional'),
 (1, 'Tecnico'),
 (2, 'Media'),
 (3, 'Basica'),
 (4, 'No Posee');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `estadocivil`
---
-
 CREATE TABLE `estadocivil` (
   `estadocivil_id` int(11) NOT NULL,
   `estadocivil_tipo` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `estadocivil`
---
 
 INSERT INTO `estadocivil` (`estadocivil_id`, `estadocivil_tipo`) VALUES
 (0, 'Soltero'),
@@ -114,14 +63,9 @@ INSERT INTO `estadocivil` (`estadocivil_id`, `estadocivil_tipo`) VALUES
 (2, 'Viudo'),
 (3, 'Divorciado');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `postulante`
---
-
 CREATE TABLE `postulante` (
   `postulante_rut` varchar(12) NOT NULL,
+  `postulante_dv`  char(1) NOT NULL,
   `postulante_nombre` varchar(25) DEFAULT NULL,
   `postulante_apepa` varchar(25) NOT NULL,
   `postulante_apema` varchar(25) NOT NULL,
@@ -133,17 +77,11 @@ CREATE TABLE `postulante` (
   `postulante_direccion` varchar(50) NOT NULL,
   `postulante_sueldo` int(11) NOT NULL,
   `postulante_enfermedad` char(1) NOT NULL,
-  `educacione_educacione_id` int(11) NOT NULL,
+  `educacion_educacion_id` int(11) NOT NULL,
   `estadocivil_estadocivil_id` int(11) NOT NULL,
   `comuna_comuna_id` int(11) NOT NULL,
-  `venta_venta_id` int(11) NOT NULL
+  `renta_renta_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `usuario`
---
 
 CREATE TABLE `usuario` (
   `usuario_id` int(11) NOT NULL,
@@ -152,84 +90,36 @@ CREATE TABLE `usuario` (
   `usuario_roladm` char(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `venta`
---
-
-CREATE TABLE `venta` (
-  `venta_id` int(11) NOT NULL,
-  `venta_tipo` varchar(25) NOT NULL
+CREATE TABLE `renta` (
+  `renta_id` int(11) NOT NULL,
+  `renta_tipo` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `venta`
---
-
-INSERT INTO `venta` (`venta_id`, `venta_tipo`) VALUES
+INSERT INTO `renta` (`renta_id`, `renta_tipo`) VALUES
 (0, 'Fija'),
 (1, 'Variable'),
 (2, 'Boleta de Honorarios');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `comuna`
---
 ALTER TABLE `comuna`
   ADD PRIMARY KEY (`comuna_id`);
 
---
--- Indexes for table `educacione`
---
-ALTER TABLE `educacione`
-  ADD PRIMARY KEY (`educacione_id`);
+ALTER TABLE `educacion`
+  ADD PRIMARY KEY (`educacion_id`);
 
---
--- Indexes for table `estadocivil`
---
 ALTER TABLE `estadocivil`
   ADD PRIMARY KEY (`estadocivil_id`);
 
---
--- Indexes for table `postulante`
---
 ALTER TABLE `postulante`
-  ADD PRIMARY KEY (`postulante_rut`),
-  ADD KEY `postulante_comuna_fk` (`comuna_comuna_id`),
-  ADD KEY `postulante_educacione_fk` (`educacione_educacione_id`),
-  ADD KEY `postulante_estadocivil_fk` (`estadocivil_estadocivil_id`),
-  ADD KEY `postulante_venta_fk` (`venta_venta_id`);
+  ADD PRIMARY KEY (`postulante_rut`);
 
---
--- Indexes for table `usuario`
---
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`usuario_id`);
 
---
--- Indexes for table `venta`
---
-ALTER TABLE `venta`
-  ADD PRIMARY KEY (`venta_id`);
+ALTER TABLE `renta`
+  ADD PRIMARY KEY (`renta_id`);
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `postulante`
---
 ALTER TABLE `postulante`
   ADD CONSTRAINT `postulante_comuna_fk` FOREIGN KEY (`comuna_comuna_id`) REFERENCES `comuna` (`comuna_id`),
-  ADD CONSTRAINT `postulante_educacione_fk` FOREIGN KEY (`educacione_educacione_id`) REFERENCES `educacione` (`educacione_id`),
+  ADD CONSTRAINT `postulante_educacion_fk` FOREIGN KEY (`educacion_educacion_id`) REFERENCES `educacion` (`educacion_id`),
   ADD CONSTRAINT `postulante_estadocivil_fk` FOREIGN KEY (`estadocivil_estadocivil_id`) REFERENCES `estadocivil` (`estadocivil_id`),
-  ADD CONSTRAINT `postulante_venta_fk` FOREIGN KEY (`venta_venta_id`) REFERENCES `venta` (`venta_id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  ADD CONSTRAINT `postulante_renta_fk` FOREIGN KEY (`renta_renta_id`) REFERENCES `renta` (`renta_id`);
